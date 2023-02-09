90b8075d8aeb51bafca14cf302db14b0adba48a5
Greensebas <greensebas@hotmail.com>
2023-02-09 00:00:06 -0300

    todo.store functions

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -6,6 +6,7 @@ const Filters = {
     Pending: 'Pending'
 };
 
+
 const state = {
     todos: [
         new Todo('Piedra del alma'),
@@ -15,11 +16,69 @@ const state = {
     filter: Filters.All,
 };
 
+
 const initStore = () => {
     console.log(state);
     console.log('InitStore 🥑');
 };
 
+
+const loadStore = () => {
+    throw new Error('Not implemented');
+};
+
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+};
+
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    
+    state.todos = state.todos.map( todo => {
+        
+    })
+};
+
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+};
+
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+};
+
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+};
+
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+};
+
 export default { 
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
